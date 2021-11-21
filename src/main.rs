a9f5278dad92a27be21cf7a112e98bf65cfa1b8c
Guillaume de Cagny <gdc@zint.app>
2021-11-21 13:56:28 +0100

    move to asynchronous, before trying to read ttyin

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+
+
 #[macro_use]
 extern crate fstrings;
 
 extern crate base64;
 
+
+use futures::pin_mut;
 use std::env;
-use std::io;
+use std::pin::Pin;
 use rand::{Rng};
-use std::io::prelude::*;
-use std::fs::{self, OpenOptions};
 use fstrings::f;
 
+
+use tokio::fs::{File, OpenOptions};
+use tokio::io::AsyncWriteExt;
+use tokio::time::sleep;
+
+use std::time::Duration;
+
+
+
+use async_std::io::{stdin, stdout, ReadExt, WriteExt, Stdout};
 
 const PREFIX: &str = "\x1bPtmux;\x1b\x1bP?";
 const SUFFIX: &str = "\x1b\\\x1b\\";
@@ -28,10 +40,16 @@ fn build_close_payload(random_id: i32) -> String {
 }
 
 
-fn main() {
-    let mut stdin = io::stdin();
-    let mut ttyout = OpenOptions::new().write(true).open("/dev/tty").expect("unable to open /dev/tty for writing");
-    
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>  {
+    let mut stdin = stdin();
+    let mut stdout = stdout();
+
+    let ttyout = OpenOptions::new().write(true)/* .create(true) */.open("/dev/tty").await?;
+
+    pin_mut!(ttyout);
+        
+
     let mut buf: [u8; 65536] = [0; 65536];
 
     let random_id_u: u32 = rand::thread_rng().gen();
@@ -39,18 +57,35 @@ fn main() {
 
     let command = env::args().skip(1).collect::<Vec<String>>().join(" ");
 
-    ttyout.write(build_command_payload(random_id, &command).as_bytes());
+    ttyout.write_all(build_command_payload(random_id, &command).as_bytes()).await?;
 
     loop {
+        match stdin.read(&mut buf).await? {
+            0 => {
+                debug(&mut stdout, &mut ttyout, "received 0 bytes!\n".as_bytes()).await?;
 
-        match stdin.read(&mut buf) {
-            Ok(0) => { break; }
-            Ok(i) => { 
-                ttyout.write(build_payload(random_id, 2, &buf[..i]).as_bytes());
+                break;
             }
-            Err(e) => { panic!("{}", e) }
+            i => { 
+                debug(&mut stdout, &mut ttyout, f!("received {i} bytes!\n").as_bytes()).await?;
+                match ttyout.write_all(build_payload(random_id, 2, &buf[..i]).as_bytes()).await {
+                    Ok(()) => {}
+                    Err(e) => { panic!("Error on write! {}", e)}
+                }
+            }
         }
     }
+    
+    //stdout.write(f!("printing closing payload!\n").as_bytes()).await?;
 
-    ttyout.write(build_close_payload(random_id).as_bytes());
+    ttyout.write_all(build_close_payload(random_id).as_bytes()).await?;
+    ttyout.flush().await?;
+    Ok(())
+}
+
+async fn debug(stdout: &mut Stdout, ttyout: &mut Pin<&mut File>, buf: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    ttyout.flush().await?;
+    stdout.write(buf).await?;
+    stdout.flush().await?;
+    Ok(())
 }
